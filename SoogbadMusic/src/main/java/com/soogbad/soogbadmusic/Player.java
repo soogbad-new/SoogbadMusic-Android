82101721aaaf151af6a10478bc359e7477926091
Sagi Klein <soogbad@gmail.com>
2026-05-13 10:19:25 +0300

    media3 player

--- SoogbadMusic/src/main/java/com/soogbad/soogbadmusic/Player.java
+++ SoogbadMusic/src/main/java/com/soogbad/soogbadmusic/Player.java
@@ -1,44 +1,43 @@
 package com.soogbad.soogbadmusic;
 
-import android.media.AudioAttributes;
-import android.media.MediaPlayer;
+import androidx.annotation.NonNull;
+import androidx.media3.common.AudioAttributes;
+import androidx.media3.common.C;
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.PlaybackException;
+import androidx.media3.exoplayer.ExoPlayer;
 import android.net.Uri;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Player {
 
     private final Song song;
     private boolean stopped = false;
-    private final MediaPlayer mediaPlayer;
+    private final ExoPlayer mediaPlayer;
 
     private final ArrayList<Utility.EmptyListener> onPlaybackCompletedListeners = new ArrayList<>();
     public void addOnPlaybackCompletedListener(Utility.EmptyListener listener) {
         onPlaybackCompletedListeners.add(listener);
     }
 
-    /** @noinspection CallToPrintStackTrace*/
     public Player(Song song) {
         this.song = song;
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioAttributes(new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA).setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build());
-        mediaPlayer.setOnCompletionListener(mediaPlayer -> {
-            stopped = true;
-            for(Utility.EmptyListener listener : onPlaybackCompletedListeners)
-                listener.onListenerInvoked();
+        mediaPlayer = new ExoPlayer.Builder(SoogbadMusicApplication.getAppContext()).build();
+        mediaPlayer.setAudioAttributes(new AudioAttributes.Builder().setUsage(C.USAGE_MEDIA).setContentType(C.AUDIO_CONTENT_TYPE_MUSIC).build(), true);
+        mediaPlayer.addListener(new androidx.media3.common.Player.Listener() {
+            @Override public void onPlaybackStateChanged(int playbackState) {
+                androidx.media3.common.Player.Listener.super.onPlaybackStateChanged(playbackState);
+                if(playbackState == androidx.media3.common.Player.STATE_ENDED)
+                    notifyCompletion();
+            }
+            @Override public void onPlayerError(@NonNull PlaybackException error) {
+                androidx.media3.common.Player.Listener.super.onPlayerError(error);
+                notifyCompletion();
+            }
         });
-        mediaPlayer.setOnErrorListener((mediaPlayer, errorType, extraErrorCode) -> {
-            stopped = true;
-            for(Utility.EmptyListener listener : onPlaybackCompletedListeners)
-                listener.onListenerInvoked();
-            return false;
-        });
-        try {
-            mediaPlayer.setDataSource(SoogbadMusicApplication.getAppContext(), Uri.fromFile(song.getFile()));
-            mediaPlayer.prepare();
-        }
-        catch(IOException e) { e.printStackTrace(); }
+        mediaPlayer.setMediaItem(MediaItem.fromUri(Uri.fromFile(song.getFile())));
+        mediaPlayer.prepare();
     }
 
     public Song getSong() {
@@ -57,7 +56,7 @@ public class Player {
     }
 
     public void play() {
-        mediaPlayer.start();
+        mediaPlayer.play();
     }
     public void pause() {
         mediaPlayer.pause();
@@ -66,6 +65,12 @@ public class Player {
     public void release() {
         mediaPlayer.stop();
         mediaPlayer.release();
+    }
+
+    private void notifyCompletion() {
+        stopped = true;
+        for(Utility.EmptyListener listener : onPlaybackCompletedListeners)
+            listener.onListenerInvoked();
     }
 
 }
